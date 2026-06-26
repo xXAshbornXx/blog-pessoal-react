5ae5748a25b6ca868800100fd12af11d2f8d2c96
Igor N. S. <guigasilvaferreira@gmail.com>
2026-06-26 16:26:22 -0300

    feat: adiciona páginas de login, cadastro e roteamento
    - Configuração do react-router-dom no App.js
    - Implementação da página de Login na rota raiz ('/')
    - Implementação da página de Cadastro com formulário estruturado
    - Criação do componente Footer com links para redes sociais

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import Footer from './components/footer/Footer';
-import Navbar from './components/navbar/Navbar';
-import Home from './pages/home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
+import Cadastro from './pages/cadastro/Cadastro'
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Home />
-      <Footer />
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -13,9 +13,15 @@ function Footer() {
           </p>
           <p className='text-lg'>Acesse nossas redes sociais</p>
           <div className='flex gap-2'>
-            <LinkedinLogoIcon size={48} weight='bold' />
-            <InstagramLogoIcon size={48} weight='bold' />
-            <FacebookLogoIcon size={48} weight='bold' />
+            <a href="https://www.linkedin.com/in/seu_usuario" target="_blank">
+              <LinkedinLogoIcon size={48} weight='bold' />
+            </a>
+            <a href="https://www.instagram.com/seu_usuario" target="_blank">
+              <InstagramLogoIcon size={48} weight='bold' />
+            </a>
+            <a href="https://www.facebook.com/seu_usuario" target="_blank">
+              <FacebookLogoIcon size={48} weight='bold' />
+            </a>
           </div>
         </div>
       </div>
