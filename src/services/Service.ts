4bff02585b85ddf3fa96ce3e3e5790d3cc743a16
Igor N. S. <guigasilvaferreira@gmail.com>
2026-07-06 12:00:05 -0300

    Deploy do Projeto Blog Pessoal

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { AxiosRequestConfig } from "axios";
 
 const api = axios.create({
-  baseURL: "https://blogpessoal-avjw.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 export const cadastrarUsuario = async (
